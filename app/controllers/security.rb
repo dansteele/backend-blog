88b31a86b9be0fb1753b9a3a89ddaf30529c6ac9
dansteele <danielsteele@hotmail.co.uk>
2015-02-09 21:08:12 +0000

    Created login form and signup controller code

--- app/controllers/security.rb
+++ app/controllers/security.rb
@@ -5,7 +5,15 @@ BackendBlog::App.controllers :security do
   end
 
   post :login do
-    #Check if in DB
+  user = User.find_by_username(params[:username])
+    unless user.nil?
+      if params[:password] == user.password
+        session[:logged_in] = true
+        redirect url_for(:people, :all)
+      end
+    end
+  flash[:bad_login] = "Bad login."
+  redirect url_for(:people, :login)
   end
 
   get :signup  do
